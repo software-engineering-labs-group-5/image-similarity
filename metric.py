d2aa0b8357bb49a40a50a7ff320282c287ea9aa8
Blednaowca <44226089+Blednaowca@users.noreply.github.com>
2020-11-26 20:54:27 +0100

    working metric and loading

--- metric.py
+++ metric.py
@@ -1,5 +1,31 @@
-class Metric():
-	str: name
+import numpy as np
+import glob
+import metric
+import importlib
+import os
+import inspect
 
-	def __init__(name):
+class Metric():
+	def __init__(self, name, computation):
 		self.name = name
+		self.compute = computation
+	
+def load_metrics():
+	metrics_list = []
+	path = os.getcwd()
+	path = path + '\metrics'
+	plugins = os.listdir(path)
+
+	for plugin in plugins:
+		if '_metric.py' not in plugin:
+			plugins.remove(plugin)
+			continue
+		module_str = 'metrics.' + plugin
+		module_str = module_str[:-3] 
+		module = importlib.import_module(module_str)
+
+		functions = inspect.getmembers(module, inspect.isfunction)
+		name, function = functions[0]
+
+		metrics_list.append(metric.Metric(name, function))
+	return metrics_list
