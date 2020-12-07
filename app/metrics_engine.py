8f482ff4d42a424c8fe16d31b807a4325008b29b
Kasper Wiśniowski <44226089+Blednaowca@users.noreply.github.com>
2020-12-07 20:04:51 +0100

    Metrics (#36)
    * Added Exception( "No plugins folder found" )
    
    * Added Exception ("No plugin in plugins folder")
    
    * Polished metrics loading
    
    * Addad fsim metric, test doesn't work
    
    * cpsnr kinda implemented
    
    * Example array for tests
    
    * Givingg back fsim
    
    * Added calculate_metrics
    
    * Polish calculate_metrics
    
    * Add default plugins path
    
    Co-authored-by: Kamil Klonowski <40344063+Pepzik@users.noreply.github.com>

--- app/metrics_engine.py
+++ app/metrics_engine.py
@@ -10,19 +10,47 @@ class MetricsEngine():
 		self.metrics_list = []
 		pass
 
-	def load_metrics(self, path):
-		plugins = os.listdir(path)
+    	def load_metrics(self, path=""):
+		if path == "":
+			path = os.path.join(os.path.dirname(os.getcwd()), "plugins")
+		try:
+			plugins_list = os.listdir(path)
+		except:
+			try:
+				path = path[1:]
+				plugins_list = os.listdir(path)
+			except:
+				try:
+					path = path[2:]
+					plugins_list = os.listdir(path)
+				except:
+					raise Exception("No plugins folder found!")
+		
+		plugins = []
+		for plugin in plugins_list:
+			if '_metric.py' in plugin:
+				if not os.path.isdir(plugin):
+					plugins.append(plugin)
+		
+		if not len(plugins) > 0:
+			raise Exception("No plugin in plugins folder found!") 
 
 		for plugin in plugins:
-			if '_metric.py' not in plugin:
-				plugins.remove(plugin)
-				continue
 			module_str = f"{path.replace('/', '')}"
+			module_str = f"{module_str.replace('.', '')}"
 			spec = importlib.util.spec_from_file_location(module_str, f"{path}/{plugin}")
 			module = importlib.util.module_from_spec(spec)
 			spec.loader.exec_module(module)
 
 			functions = inspect.getmembers(module, inspect.isfunction)
-			name, function = functions[0]
+
+			for func in functions:
+				name, function = func
+				if '_metric' in name:
+					break
 
 			self.metrics_list.append(Metric(name, function))
+
+	def calculate_metrics(self, reference_image, modified_image):
+		results = {metric.name: metric.compute(reference_image, modified_image) for metric in self.metrics_list}
+		return results
--- app/metrics_engine_test.py
+++ app/metrics_engine_test.py
@@ -1,38 +1,58 @@
 import numpy as np
+import time
 from metrics_engine import MetricsEngine
 
 def test_load_metrics():
 	# given
 	engine = MetricsEngine()
-	expected_metrics_names = ['psnr_metric', 'rmse_metric']
+	expected_metrics_names = sorted(['cpsnr_metric', 
+									'psnr_metric',
+									'rmse_metric',
+									'fsim_metric', 
+									'ssim_metric'])
 
 	# when
 	engine.load_metrics('../plugins')
 
 	# then
-	assert len(engine.metrics_list) == 2
-	metrics_names = [metric.name for metric in engine.metrics_list]
+	assert len(engine.metrics_list) == 5
+	metrics_names = sorted([metric.name for metric in engine.metrics_list])
+	print(metrics_names)
 	assert metrics_names == expected_metrics_names
 	print(f"test_load_metrics: pass")
 
 
 def test_metric_compute():
 	# given
-	img_1 = np.zeros((3,3,3))
-	img_2 = np.ones((3,3,3))
+	# img_1 = np.zeros((300,300,3))
+	# img_2 = np.ones((300,300,3))
+
+	#img_1 = np.random.random_sample((300,300,3))
+
+	img_1a = np.genfromtxt('example_array_0.csv', delimiter=',')
+	img_1b = np.genfromtxt('example_array_1.csv', delimiter=',')
+	img_1c = np.genfromtxt('example_array_2.csv', delimiter=',')
+	img_1 = np.stack([img_1a, img_1b, img_1c], axis=-1)
+	img_2 = img_1 * 1.05
+
 
 	engine = MetricsEngine()
 	engine.load_metrics('../plugins')
 
 	expected_results = {
-		'psnr_metric': 72.24507812192874,
-		'rmse_metric': 0.0002442002442002442
+		'cpsnr_metric': 36.81357101978457,
+		'fsim_metric': 0.9992996404431441,
+		'psnr_metric': 103.0365991951924,
+		'rmse_metric': 7.04969032961156e-06,
+		'ssim_metric': 0.9999996107525898
 	}
 
 	# when
+	start = time.time()
 	results = {metric.name: metric.compute(img_1, img_2) for metric in engine.metrics_list}
-
+	end = time.time()
 	# then
+	print(f"Metrics calculation took: {end-start} seconds")
 	assert results == expected_results
 	print(f"test_metric_compute: pass")
 
