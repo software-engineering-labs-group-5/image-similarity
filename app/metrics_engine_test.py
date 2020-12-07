5bfba044f6ec8df96cc1f283ce2a57f920bc8261
Kasper Wisniowski <kaswisniowski@gmail.com>
2020-12-07 18:08:52 +0100

    Example array for tests

--- app/metrics_engine_test.py
+++ app/metrics_engine_test.py
@@ -1,11 +1,15 @@
 import numpy as np
-
+import time
 from metrics_engine import MetricsEngine
 
 def test_load_metrics():
 	# given
 	engine = MetricsEngine()
-	expected_metrics_names = sorted(['cpsnr_metric', 'psnr_metric', 'rmse_metric', 'fsim_metric', 'ssim_metric'])
+	expected_metrics_names = sorted(['cpsnr_metric', 
+									'psnr_metric',
+									'rmse_metric',
+									'fsim_metric', 
+									'ssim_metric'])
 
 	# when
 	engine.load_metrics('../plugins')
@@ -23,29 +27,32 @@ def test_metric_compute():
 	# img_1 = np.zeros((300,300,3))
 	# img_2 = np.ones((300,300,3))
 
-	img_1 = np.random.random_sample((300,300,3))
+	#img_1 = np.random.random_sample((300,300,3))
 
-	img_2 = img_1 * 128
+	img_1a = np.genfromtxt('example_array_0.csv', delimiter=',')
+	img_1b = np.genfromtxt('example_array_1.csv', delimiter=',')
+	img_1c = np.genfromtxt('example_array_2.csv', delimiter=',')
+	img_1 = np.stack([img_1a, img_1b, img_1c], axis=-1)
+	img_2 = img_1 * 1.05
 
 
 	engine = MetricsEngine()
 	engine.load_metrics('../plugins')
 
 	expected_results = {
-		'fsim_metric': np.float64('nan'),
-		'psnr_metric': 72.24507812192874,
-		'rmse_metric': 0.0002442002442002442,
-		'ssim_metric': 0.9994040178139078
+		'cpsnr_metric': 36.81357101978457,
+		'fsim_metric': 0.9992996404431441,
+		'psnr_metric': 103.0365991951924,
+		'rmse_metric': 7.04969032961156e-06,
+		'ssim_metric': 0.9999996107525898
 	}
 
 	# when
+	start = time.time()
 	results = {metric.name: metric.compute(img_1, img_2) for metric in engine.metrics_list}
-	print(results)
-	print(expected_results)
-	print(type(results['fsim_metric']))
-	print(type(expected_results['fsim_metric']))
+	end = time.time()
 	# then
-
+	print(f"Metrics calculation took: {end-start} seconds")
 	assert results == expected_results
 	print(f"test_metric_compute: pass")
 
--- plugins/cpsnr_metric.py
+++ plugins/cpsnr_metric.py
@@ -10,12 +10,12 @@ def cpsnr_metric(sr, hr):
 	Returns:
 		cPSNR: float, score
 	"""
-	hr_map = np.ones((hr.shape[0], hr.shape[1], hr.shape[2]))
+	#hr_map = np.ones((hr.shape[0], hr.shape[1], hr.shape[2]))
 
 	if len(sr.shape) == 2:
 		sr = sr[None, ]
 		hr = hr[None, ]
-		hr_map = hr_map[None, ]
+		#hr_map = hr_map[None, ]
 
 	if sr.dtype.type is np.uint16:  # integer array is in the range [0, 65536]
 		sr = sr / np.iinfo(np.uint16).max  # normalize in the range [0, 1]
@@ -24,10 +24,17 @@ def cpsnr_metric(sr, hr):
 	if hr.dtype.type is np.uint16:
 		hr = hr / np.iinfo(np.uint16).max
 
-	n_clear = np.sum(hr_map, axis=(1, 2))  # number of clear pixels in the high-res patch
+	n_clear = hr.shape[0] * hr.shape[0]
+	#n_clear = np.sum(hr_map, axis=(1, 2))  # number of clear pixels in the high-res patch
 	diff = hr - sr
-	bias = np.sum(diff * hr_map, axis=(1, 2)) / n_clear  # brightness bias
-	cMSE = np.sum(np.square((diff - bias[:, None, None]) * hr_map), axis=(1, 2)) / n_clear
+	#bias = np.sum(diff * hr_map, axis=(1, 2)) / n_clear  # brightness bias
+	bias = np.sum(diff, axis=(0, 1)) / n_clear
+
+	cMSE = []
+	for i in range(bias.shape[0]):
+		cMSE.append(np.sum(np.square((diff[:,:,i] - bias[i])), axis=(0, 1)) / n_clear)
+
+	#cMSE = np.sum(np.square((diff - bias[:, None, None])), axis=(0, 1)) / n_clear
 	cPSNR = -10 * np.log10(cMSE)  # + 1e-10)
 
 	cPSNR = np.mean(cPSNR)
