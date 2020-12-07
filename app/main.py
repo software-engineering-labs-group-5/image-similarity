2c4f465566476a0574c8538b855c94194e6c9a45
maciwid <44189693+maciwid@users.noreply.github.com>
2020-12-07 19:34:18 +0100

    Implemented 'Controls' class #7 (#35)
    Moved image_handler and metrics_provider functionality from main_view to controls

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from image_handler import ImageHandler
 from modifications_provider import ModificationsProvider
+from controls import Controls
 from main_view import *
 
 
@@ -7,14 +8,18 @@ def main():
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
     ui = Ui_MainWindow()
+    controls = Controls()
+    ui.subscribe_controls(controls)
     ui.setupUi(MainWindow)
 
+    
+    controls.subscribe_view(ui)
     image_handler = ImageHandler()
     image_handler.subscribe_view(ui)
     modifications_provider = ModificationsProvider()
     modifications_provider.subscribe_image_handler(image_handler)
-    ui.subscribe_image_handler(image_handler)
-    ui.subscribe_modifications_provider(modifications_provider)
+    controls.subscribe_image_handler(image_handler)
+    controls.subscribe_modifications_provider(modifications_provider)    
 
     MainWindow.show()
     sys.exit(app.exec_())
