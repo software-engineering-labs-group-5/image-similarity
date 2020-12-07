c74383c84cd99092acf3b65a920b4f6e4ded9209
Kamil Klonowski <40344063+Pepzik@users.noreply.github.com>
2020-12-08 00:24:44 +0100

    Connect metrics calculation with calculate metrics button, add minor fixes (#37)
    * Move fsim_metric.py to subfolder 'additional'
    
    * Change image matrix type - fix metrics calculation error
    
    * Connect matrics calculation with calculate metrics button, fix formatting error

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from image_handler import ImageHandler
 from modifications_provider import ModificationsProvider
 from controls import Controls
+from metrics_engine import MetricsEngine
 from main_view import *
 
 
@@ -12,14 +13,17 @@ def main():
     ui.subscribe_controls(controls)
     ui.setupUi(MainWindow)
 
-    
+    metrics_engine = MetricsEngine()
+    metrics_engine.subscribe_view(ui)
+    metrics_engine.load_metrics()
     controls.subscribe_view(ui)
     image_handler = ImageHandler()
     image_handler.subscribe_view(ui)
     modifications_provider = ModificationsProvider()
     modifications_provider.subscribe_image_handler(image_handler)
     controls.subscribe_image_handler(image_handler)
-    controls.subscribe_modifications_provider(modifications_provider)    
+    controls.subscribe_modifications_provider(modifications_provider)
+    image_handler.subscribe_metrics_engine(metrics_engine)
 
     MainWindow.show()
     sys.exit(app.exec_())
